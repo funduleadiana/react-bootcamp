3ee91214f7926f94465615187b02cf3a8cf7fc76
funduleadiana <diana.fundulea@yahoo.com>
2021-07-15 14:17:47 +0100

    passing test to show rows

--- calculator/src/components/Calculator.tsx
+++ calculator/src/components/Calculator.tsx
@@ -1,11 +1,20 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
+
 const Calculator = () => {
   return (
     <div className="calculator">
       <h1>Calculator</h1>
-      {numbers.map((n) => (
-        <button key={n}>{n.toString()}</button>
-      ))}
+      <div role="grid">
+        {rows.map((row) => {
+          return (
+            <div key={row.toString()} role="row">
+              {row.map((n) => (
+                <button key={n}>{n.toString()}</button>
+              ))}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
