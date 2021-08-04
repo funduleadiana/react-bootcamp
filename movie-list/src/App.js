ffb0d39a08aaa0087b499ab47c8b52c8129cf6b7
funduleadiana <diana.fundulea@yahoo.com>
2021-08-04 21:00:41 +0100

    setting up Header component

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MoviePage from "./components/MoviePage/MoviePage";
+import Header from "./components/Header/Header";
 import "./App.css";
 
 // const API_MOVIES =`https://api.themoviedb.org/3/discover/movie?api_key=c857fa67fba523ad3ce66df18e7ab279&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
@@ -30,6 +31,7 @@ class App extends React.Component {
     console.log(movies);
     return (
       <div className="App">
+        <Header />
         {movies.map((movie) => (
           <MoviePage
             key={movie.id}
